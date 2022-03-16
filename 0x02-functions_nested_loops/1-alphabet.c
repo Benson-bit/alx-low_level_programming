33521db4abd65eba2cda4e6419c6bc1323b8f7ce
Benson-bit <benchege71@gmail.com>
2022-03-16 13:22:44 -0700

    trial

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+
+
+Void print_alphabet(void) 
+{
+Char alp[26]=”abcdefghijklmnopqrstuvwxyz”;
+Int i;
+
+For (i = 0; i < 26; i++);
+{
+print(alp[i]);
+}
+print(“\n”);
+Return 0;
+}
